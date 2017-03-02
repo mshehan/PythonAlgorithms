ac533e37116d88c2869b7b8910b4b0f67eb4dd43
mshehan <mshehan@my.smccd.edu>
2017-03-02 12:05:58 -0800

    edited some variable names

--- dynamic_practice.py
+++ dynamic_practice.py
@@ -1,25 +1,25 @@
-C=[0,11, 9, 9, 12, 12, 12, 12, 9, 9, 11]
+s=[0,11, 9, 9, 12, 12, 12, 12, 9, 9, 11]
 val = {}
 pattern = {}
 def OPT(i):
   r = 1
-  c = 40
-  if(i >= len(c)):
-    print("cannot be calculated with the following")
-    
+  s = 40
+  if(i >= len(s)):
+    print("cannot be calsulated with the following")
+
   if i == 0:
     val[0] = 0
     return val[0]
   if i>= 1 and i < 4:
     if not i in val:
-      val[i] = (r*C[i] + OPT(i-1))
+      val[i] = (r*s[i] + OPT(i-1))
       pattern[i] = 'b'
     pattern[i] = 'a'
     return val[i]
   if i >= 4:
-    val[i] = min((r*C[i] + OPT(i-1)),(c+OPT(i-4)))
+    val[i] = min((r*s[i] + OPT(i-1)),(s+OPT(i-4)))
     pattern[i] = 'b' if(val[i] == c+OPT(i-4)) else 'a'
-      
+
     return val[i]
 
 OPT(10)
