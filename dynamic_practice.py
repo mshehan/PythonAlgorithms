34e6037d019d6501a38b0f2be856aca81eef0587
mshehan <mshehan@my.smccd.edu>
2017-03-05 22:34:17 -0800

    errors corrected

--- dynamic_practice.py
+++ dynamic_practice.py
@@ -1,10 +1,10 @@
-s=[0,11, 9, 9, 12, 12, 12, 12, 9, 9, 11]
+S=[0,11, 9, 9, 12, 12, 12, 12, 9, 9, 11]
 val = {}
 pattern = {}
 def OPT(i):
   r = 1
-  s = 40
-  if(i >= len(s)):
+  c = 40
+  if(i >= len(S)):
     print("cannot be calsulated with the following")
 
   if i == 0:
@@ -12,12 +12,12 @@ def OPT(i):
     return val[0]
   if i>= 1 and i < 4:
     if not i in val:
-      val[i] = (r*s[i] + OPT(i-1))
+      val[i] = (r*S[i] + OPT(i-1))
       pattern[i] = 'b'
     pattern[i] = 'a'
     return val[i]
   if i >= 4:
-    val[i] = min((r*s[i] + OPT(i-1)),(s+OPT(i-4)))
+    val[i] = min((r*S[i] + OPT(i-1)),(c+OPT(i-4)))
     pattern[i] = 'b' if(val[i] == c+OPT(i-4)) else 'a'
 
     return val[i]
